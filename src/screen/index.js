cf828d81fa6b3f4846f36dd2788ac778b32b7992
Afrizal <afrizalbagas.03@gmail.com>
2021-08-23 08:02:42 +0700

    Add new screen

---
+++ src/screen/index.js
@@ -0,0 +1,5 @@
+import Splashscreen from './Splashscreen';
+import LoginScreen from './LoginScreen';
+import RegisterScreen from './RegisterScreen';
+
+export {Splashscreen, LoginScreen, RegisterScreen};
