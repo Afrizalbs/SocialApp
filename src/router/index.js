31fb3a29654153ceabf01d1de88b86568e128be1
Afrizal <afrizalbagas.03@gmail.com>
2021-08-26 20:32:29 +0700

    Handle user

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,31 @@
-import {createStackNavigator} from '@react-navigation/stack';
-import React from 'react';
-import {LoginScreen, RegisterScreen, Splashscreen} from '../screen';
+import {NavigationContainer} from '@react-navigation/native';
+import React, {useContext, useState, useEffect} from 'react';
+import auth from '@react-native-firebase/auth';
+import {AuthContext} from './AuthProvider';
 
-const Stack = createStackNavigator();
+import AuthStack from './AuthStack';
+import AppStack from './AppStack';
 
 const Router = () => {
+  const {user, setUser} = useContext(AuthContext);
+  const [initializing, setInitializing] = useState(true);
+
+  // Handle user state changes
+  const onAuthStateChanged = user => {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  };
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <Stack.Navigator headerMode="none" initialRouteName="LoginScreen">
-      <Stack.Screen name="SplashScreen" component={Splashscreen} />
-      <Stack.Screen name="LoginScreen" component={LoginScreen} />
-      <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-    </Stack.Navigator>
+    <NavigationContainer>
+      {user ? <AppStack /> : <AuthStack />}
+    </NavigationContainer>
   );
 };
 
