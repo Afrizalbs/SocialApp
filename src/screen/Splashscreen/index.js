4620660422432aa0d21c3a00ef62f9b1b47261d7
Afrizal <afrizalbagas.03@gmail.com>
2021-08-26 20:25:20 +0700

    Navigation to LoginScreen

--- src/screen/Splashscreen/index.js
+++ src/screen/Splashscreen/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Image, SafeAreaView, StyleSheet} from 'react-native';
 import {ILLogo} from '../../asset';
 
-const Splashscreen = () => {
+const Splashscreen = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.replace('LoginScreen');
+    }, 1500);
+  });
   return (
     <SafeAreaView style={styles.page}>
       <Image source={ILLogo} style={styles.logo} />
